18ea195c3dae56818f0dffde993b2fd7d9dc1a77
nicotorresel <nicotorresel@gmail.com>
2021-04-09 21:27:37 -0300

    add another eslint config

--- src/application/database-provider.jsx
+++ src/application/database-provider.jsx
@@ -1,5 +1,5 @@
-import {createContext } from 'react';
-import Loki from 'lokijs';
+import { createContext } from "react";
+import Loki from "lokijs";
 
 //export const db = new Loki('example.db');
 
@@ -8,9 +8,9 @@ import Loki from 'lokijs';
 export const db = {
   error: "login",
   typeError: "usuario",
-  quantity: 3
-}
+  quantity: 3,
+};
 
-const DataBaseContext = createContext(db)
+const DataBaseContext = createContext(db);
 
 export default DataBaseContext;
